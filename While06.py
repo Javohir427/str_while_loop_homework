e263300f4c3b69b0df8d73cc0d8bee2a8e5de677
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-02 21:18:44 +0500

    06

--- While06.py
+++ While06.py
@@ -7,3 +7,14 @@ def main(s):
     Returns:
         int: return answer
     """
+    i = 0
+    n = 0
+    
+    while i<len(s):
+        if s[i].isalpha():
+
+            if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u'and s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U':
+                n+=1
+        i+=1 
+    return len(s)-n  
+print(main("CodeschoolUz"))
