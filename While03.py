280676be8b36e0d2ce49e8013840ba2b578436eb
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-02 18:25:48 +0500

    3

--- While03.py
+++ While03.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    k=0
+    i=0
+    while i<len(s):
+        if s[i].isalpha() or s[i].isdigit():
+            k+=0
+        else:
+            k+=1
+        i+=1
+
+    return k
+print (main('hwqbcnxuj.,'))
