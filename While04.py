c3a0774a56e461e9e2ec6b5aa83e83272e1e9443
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-02 18:37:21 +0500

    04

--- While04.py
+++ While04.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    #isupper
+    i = 0
+    n = 0
+
+    while i < len(s):
+        if s[i].isupper():
+            n+=1
+        i+=1
+    return n
+print(main('St34fv'))
