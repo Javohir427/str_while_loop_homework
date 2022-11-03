ea36fcec64f1733c95da1fd5848953341e8f8ee5
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-03 20:37:10 +0500

    10

--- While10.py
+++ While10.py
@@ -6,4 +6,21 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    
+    i = 0
+    n = 0
+    while i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==1:
+                n+=int(s[i])
+            else :
+                n+=0
+        i+=1
+    return n 
+print(main("98421"))
+
+
+        
+            
+            
+
