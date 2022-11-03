4b698fee0841f5f0dcfa76e54a58c0b31f488e4a
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-03 20:21:34 +0500

    09

--- While09.py
+++ While09.py
@@ -6,4 +6,18 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+
+
+    i = 0
+    n = 0
+    while i<len(s):
+        if ((s[i])).isdigit():  
+            n+=int(s[i])
+        else:
+            n+=0
+        i+=1
+    return n
+
+print(main("987654"))            
+
+      
